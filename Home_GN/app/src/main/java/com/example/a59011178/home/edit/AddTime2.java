55a0f941936c3fba7f697a634b1c53aeb3ca9f3e
xxampx <59011219@kmitl.ac.th>
2018-11-16 23:13:31 +0700

    popup

---
+++ Home_GN/app/src/main/java/com/example/a59011178/home/edit/AddTime2.java
@@ -0,0 +1,26 @@
+package com.example.a59011178.home.edit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.a59011178.home.R;
+
+public class AddTime2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        //Button mAdd = (Button) findViewById(R.id.button3);
+        //mAdd.setOnClickListener(new View.OnClickListener()
+        {
+
+            //public void onClick(View v) {
+            //    Intent intent = new Intent(AddTime2.this,ItemListAdapter_equip.class);
+            //    startActivity(intent);
+             //   finish();
+            }
+        }//);
+    }
+//}
