87eb4fecd3317c46e5d4a3a5e57e2eca143c8b59
Cat <duckkinrose@msn.com>
2018-11-24 22:51:13 +0700

    change graph

--- Home_GN/app/src/main/java/com/example/a59011178/home/graph/TabGraph.java
+++ Home_GN/app/src/main/java/com/example/a59011178/home/graph/TabGraph.java
@@ -32,6 +32,7 @@ public class TabGraph extends Fragment  {
     private DatabaseHelper mHelp;
     private Spinner mGraph_spinner;
     List<PieEntry> entries = new ArrayList<PieEntry>();
+    PieChart chart;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,18 +42,61 @@ public class TabGraph extends Fragment  {
         mItemList=mHelp.getItemList();
 
         View rootView = inflater.inflate(R.layout.fragment_tabgraph, container, false);
-
-        PieChart chart = (PieChart)rootView.findViewById(R.id.pieChart);
+        chart = (PieChart)rootView.findViewById(R.id.pieChart);
         chart = initChart(chart);
 
         mGraph_spinner = (Spinner) rootView.findViewById(R.id.Graph_spinner);
         String[] spinnerArray = getResources().getStringArray(R.array.graph_type);
         ArrayAdapter<String> graphArrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_dropdown_item_1line,spinnerArray);
         mGraph_spinner.setAdapter(graphArrayAdapter);
-
         mGraph_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedText = mGraph_spinner.getSelectedItem().toString();
+                if(selectedText.equals("Total money")){
+                    entries= new ArrayList<PieEntry>();
+                    for (Item data : mItemList) {
+                      entries.add(new PieEntry(((float)data.getPower()*(float)mItemList.get(0).getTotalMoney()/3600)/1000,data.getName()));
+                    }
+                    chart.setCenterText(new SpannableString("Usage Fee (Baht)"));
+
+                }else if(selectedText.equals("Power used")){
+                    entries= new ArrayList<PieEntry>();
+                    for (Item data : mItemList) {
+                        entries.add(new PieEntry((float)data.getPower()*(float)data.getHr(),data.getName()));
+                    }
+                    chart.setCenterText(new SpannableString("Power used (Watt)"));
+                }else if(selectedText.equals("Time used")){
+                    entries= new ArrayList<PieEntry>();
+                    for (Item data : mItemList) {
+                        entries.add(new PieEntry(((float)data.getHr()/3600),data.getName()));
+                    }
+                    chart.setCenterText(new SpannableString("Time used (Hour)"));
+                }
+
+
+                PieDataSet dataSet = new PieDataSet(entries, "Name");
+                ArrayList<Integer> colors = new ArrayList<>();
+                for (int c : ColorTemplate.VORDIPLOM_COLORS)
+                    colors.add(c);
+                for (int c : ColorTemplate.JOYFUL_COLORS)
+                    colors.add(c);
+                for (int c : ColorTemplate.COLORFUL_COLORS)
+                    colors.add(c);
+                for (int c : ColorTemplate.LIBERTY_COLORS)
+                    colors.add(c);
+                for (int c : ColorTemplate.PASTEL_COLORS)
+                    colors.add(c);
+                colors.add(ColorTemplate.getHoloBlue());
+                dataSet.setColors(colors);
+                PieData pieData = new  PieData(dataSet);
+                pieData.setValueTextColor(Color.WHITE);
+                pieData.setValueTextSize(11f);
+                //  pieData.setValueTypeface(tfLight);
+                chart.setData(pieData);
+                chart.notifyDataSetChanged();
+                chart.invalidate(); // refresh
 
             }
 
@@ -62,55 +106,14 @@ public class TabGraph extends Fragment  {
             }
         });
 
-        Legend l = chart.getLegend();
-        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
-        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
-        l.setOrientation(Legend.LegendOrientation.VERTICAL);
-        l.setDrawInside(false);
-        l.setEnabled(false);
-
-        entries= new ArrayList<PieEntry>();
-        for (Item data : mItemList) {
-            entries.add(new PieEntry(data.getPower()*data.getHrPerDay()*data.getDayPerMonth()*mItemList.get(0).getTotalMoney()/10000,data.getName()));
-        }
-
-        PieDataSet dataSet = new PieDataSet(entries, "Name");
-        ArrayList<Integer> colors = new ArrayList<>();
-
-        for (int c : ColorTemplate.VORDIPLOM_COLORS)
-            colors.add(c);
-        for (int c : ColorTemplate.JOYFUL_COLORS)
-            colors.add(c);
-        for (int c : ColorTemplate.COLORFUL_COLORS)
-            colors.add(c);
-        for (int c : ColorTemplate.LIBERTY_COLORS)
-            colors.add(c);
-        for (int c : ColorTemplate.PASTEL_COLORS)
-            colors.add(c);
-
-        colors.add(ColorTemplate.getHoloBlue());
-
-        dataSet.setColors(colors);
 
 
-        PieData pieData = new  PieData(dataSet);
-        pieData.setValueTextColor(Color.WHITE);
-        pieData.setValueTextSize(11f);
-        //  pieData.setValueTypeface(tfLight);
-        chart.setData(pieData);
-        chart.invalidate(); // refresh
-        chart.setCenterText(generateCenterSpannableText());
 
         return rootView;
     }
-    private SpannableString generateCenterSpannableText() {
 
-        SpannableString s = new SpannableString("Usage Fee (Baht)");
-        return s;
-    }
 
     private PieChart initChart(PieChart chart){
-        chart.setCenterText(generateCenterSpannableText());
 
         chart.setExtraOffsets(20.f, 0.f, 20.f, 0.f);
 
@@ -138,6 +141,7 @@ public class TabGraph extends Fragment  {
             getFragmentManager().beginTransaction().detach(this).attach(this).commit();
         }
     }
+
 }
 
 
